63a7e85fa4c57c69870561e3521194d781b8438c
MaazRaeen <Maazraeen42@gmail.com>
2025-09-18 13:02:48 +0530

    Changes in portfolio

---
+++ JavaScript/guestChecker.js
@@ -0,0 +1,7 @@
+var name=["maaz", "Lareb", "Priyanshu", "Mohit", "Father"];
+var guest=prompt("What is your name");
+if(name.includes(guest)){
+    alert("Welcome ");
+}else{
+    alert("go to hell");
+}
