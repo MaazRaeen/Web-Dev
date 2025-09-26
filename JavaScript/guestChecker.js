54b4437751ccee0130f76c91c974357831423d35
MaazRaeen <Maazraeen42@gmail.com>
2025-09-26 23:24:14 +0530

    deleted a file and add some

--- JavaScript/guestChecker.js
+++
@@ -1,7 +0,0 @@
-var name=["maaz", "Lareb", "Priyanshu", "Mohit", "Father"];
-var guest=prompt("What is your name");
-if(name.includes(guest)){
-    alert("Welcome ");
-}else{
-    alert("go to hell");
-}
