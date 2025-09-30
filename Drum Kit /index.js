8b65eac52ec918b481a2f9c5a963f695951257de
MaazRaeen <Maazraeen42@gmail.com>
2025-10-01 00:17:02 +0530

    added sound to every button

--- Drum Kit /index.js
+++ Drum Kit /index.js
@@ -2,7 +2,39 @@ var noOfDrums=document.querySelectorAll(".drum").length;
 for(i=0;i<noOfDrums;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 function handleClick(){
-    var audio=new Audio('sounds/tom-1.mp3');
-    audio.play();
+    var innerHTML=this.innerHTML;
+    switch (innerHTML) {
+        case "w":
+            var audio=new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio=new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio=new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio=new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio=new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio=new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio=new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        default:
+            break;
+    }
+    
 }
 }
