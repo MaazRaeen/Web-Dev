906af29f569a3ce4e93e6f98f9595c2b29a8e192
MaazRaeen <Maazraeen42@gmail.com>
2025-09-30 23:46:31 +0530

    added background images for sounds butttons

--- Drum Kit /index.js
+++ Drum Kit /index.js
@@ -2,6 +2,7 @@ var noOfDrums=document.querySelectorAll(".drum").length;
 for(i=0;i<noOfDrums;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 function handleClick(){
-    alert ("i got clicked");
+    var audio=new Audio('sounds/tom-1.mp3');
+    audio.play();
 }
 }
