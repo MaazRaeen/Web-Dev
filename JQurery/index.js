57b064729891e759ac142917504461e0ebcea557
MaazRaeen <Maazraeen42@gmail.com>
2025-10-02 12:13:52 +0530

    added a minified version of .js in Drum kit js file

---
+++ JQurery/index.js
@@ -0,0 +1,6 @@
+/*$(document).ready(function(){
+    $("h1").css("color", "red");
+});
+this will be needed if we put our link just after CSS path. otherwise just use -> $("h1").css("color", "red");
+*/
+$("h1").css("color", "purple");
