517a0ac245a0867b8b8d81009b9b26e107e87ff3
MaazRaeen <Maazraeen42@gmail.com>
2025-10-08 01:08:21 +0530

    Made some functions

--- Simon Game /game.js
+++ Simon Game /game.js
@@ -1,1 +1,11 @@
 
+var buttonColors= ["red", "blue", "green", "yellow"];
+var GamePattern=[];
+function nextSequence(){
+    var randomNumber=Math.random();
+    randomNumber=Math.floor(randomNumber*3)+1;
+
+    var randomChosenNumber=buttonColors[randomNumber];
+    GamePattern.push(randomChosenNumber);
+}
+nextSequence();
