4bc0ca10ffab99ddb820c1ecda5198435a77bf2e
MaazRaeen <Maazraeen42@gmail.com>
2025-10-08 01:09:25 +0530

    some fixing

--- Simon Game /game.js
+++ Simon Game /game.js
@@ -1,11 +1,10 @@
-
 var buttonColors= ["red", "blue", "green", "yellow"];
-var GamePattern=[];
+var gamePattern=[];
 function nextSequence(){
     var randomNumber=Math.random();
     randomNumber=Math.floor(randomNumber*3)+1;
 
     var randomChosenNumber=buttonColors[randomNumber];
-    GamePattern.push(randomChosenNumber);
+    gamePattern.push(randomChosenNumber);
 }
 nextSequence();
