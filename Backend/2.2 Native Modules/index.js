2a36995c2a5bdcac2c945ca7c7505fdeeda76436
MaazRaeen <Maazraeen42@gmail.com>
2025-10-13 22:11:34 +0530

    read file

--- Backend/2.2 Native Modules/index.js
+++ Backend/2.2 Native Modules/index.js
@@ -1,5 +1,9 @@
 const fs=require("fs");
-fs.writeFile("message.txt", "Hello from pNodeJs!", (err)=>{
+// fs.writeFile("message.txt", "Hello from pNodeJs!", (err)=>{
+//     if(err) throw err;
+//     console.log("file saved success fully");
+// });
+fs.readFile("./message.txt","utf8", (err, data)=>{
     if(err) throw err;
-    console.log("file saved success fully");
+    console.log(data);
 });
