3a2169f8991ea9459e4bea98f9d9ab208661760d
MaazRaeen <Maazraeen42@gmail.com>
2025-10-13 23:12:21 +0530

    added some node pakages and learn some randomGenerator function

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,14 @@
+// var generateName=require('sillyname');
+/*
+import generateName from "sillyname";
+var sillyname=generateName();
+console.log(`My name is ${sillyname}.`);
+*/
+
+
+//for superhero
+import {randomSuperhero} from 'superheroes';
+var random1=randomSuperhero();
+console.log(`I am ${random1}.`);
+
+
