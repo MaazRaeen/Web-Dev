a090e1718cab96721cdcce2c869e9a5b17517bb7
MaazRaeen <Maazraeen42@gmail.com>
2025-10-18 15:14:27 +0530

    made a new file for postman request

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -3,8 +3,15 @@ const app = express();
 const port =3001;
 
 app.get("/", (req, res)=>{
-    res.send("Hello Maaz");
+    res.send("<h1>Hello Maaz</h1>");
+    // console.log(req.rawHeaders);
 });
+
+app.get("/about", (req, res)=>{
+    res.send("<h1>About Maaz</h1>");
+
+});
+
 
 app.listen(port, ()=>{
     console.log(`Server Running on port ${port}.`);
