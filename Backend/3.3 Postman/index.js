4b3a6d84707b8df5409d9572ff70a2ec10df3659
MaazRaeen <Maazraeen42@gmail.com>
2025-10-20 06:58:23 +0530

    Postman request and make a folder named 3.4

--- Backend/3.3 Postman/index.js
+++ Backend/3.3 Postman/index.js
@@ -18,15 +18,15 @@ app.post("/register", (req, res) => {
   res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
+app.put("/user/Maaz", (req, res) => {
   res.sendStatus(200);
 });
 
-app.patch("/user/angela", (req, res) => {
+app.patch("/user/Maaz", (req, res) => {
   res.sendStatus(200);
 });
 
-app.delete("/user/angela", (req, res) => {
+app.delete("/user/Maaz", (req, res) => {
   //Deleting
   res.sendStatus(200);
 });
