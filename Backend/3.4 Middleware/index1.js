4b5e7e82a51a15be88e3b7ed946a7ee4b53dd23d
MaazRaeen <Maazraeen42@gmail.com>
2025-10-20 10:51:37 +0530

    doenload a "morgan" npm

--- Backend/3.4 Middleware/index1.js
+++ Backend/3.4 Middleware/index1.js
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
@@ -5,10 +6,16 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({extended:  true}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+
+app.post("/Submit", (req, res)=>{
+  console.log(req.body );
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
