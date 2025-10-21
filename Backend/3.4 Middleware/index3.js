dbab6b1eaf36b998901db4d0e95637e5963d8477
MaazRaeen <Maazraeen42@gmail.com>
2025-10-22 01:53:23 +0530

    learn about some middlewares

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -1,8 +1,12 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
-
+function logger(req, res, next){
+  console.log(" Request Method", req.method);
+  console.log(" Request URL", req.url);
+  next();
+}
 app.use(logger);
 
 app.get("/", (req, res) => {
