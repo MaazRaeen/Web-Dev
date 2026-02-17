d4071d72f6a680847eafc900d5cef3c656fc5879
MaazRaeen <Maazraeen42@gmail.com>
2026-02-17 22:07:30 +0530

    implemented the REST API "GET , POST , PATCH , PUT , DELETE"

--- Backend/5.5 REST APIs/solution.js
+++ Backend/5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "9f49eff7-9358-476d-81e0-d3e2d51672e0";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
